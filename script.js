4fe7a7a5a79ce6fec50be8872fbb43af40329757
Abhi00045 <128639147+Abhi00045@users.noreply.github.com>
2024-06-17 19:29:31 +0530

    not now

---
+++ script.js
@@ -0,0 +1,93 @@
+let login = document.getElementById("login");
+let graphic = document.getElementById("graphic");
+let second = document.getElementById("second")
+
+login.addEventListener("click",function(){
+
+graphic.style.display="none";
+
+let div = document.createElement("div");
+let form = document.createElement("form");
+
+let emailManger =document.createElement("div");
+
+let label = document.createElement("label");
+label.innerText="Email : ";
+let input =document.createElement("input");
+input.type="Email";
+input.placeholder="abc@gmail.com";
+
+let passwordManger =document.createElement("div");
+
+let label2 = document.createElement("label");
+label2.innerText="Password : ";
+let input2 =document.createElement("input");
+input2.type="password";
+input2.placeholder="password";
+
+div.style.fontFamily="Arial, Helvetica, sans-serif";
+
+
+let loginBtn = document.createElement("Button");
+loginBtn.innerText="Log in";
+loginBtn.style.color="black";
+
+label.style.fontSize="25px";
+label.style.fontWeight="400";
+input.style.height="30px";
+input.style.width="250px";
+input.style.border=" 2px solid rgb(204, 158, 88)";
+input.style.borderRadius="10px"
+input.style.background="White";
+input.style.color="Black";
+input.style.padding="10px";
+
+label2.style.fontSize="25px";
+label2.style.fontWeight="400";
+input2.style.height="30px";
+input2.style.width="250px";
+input2.style.border=" 2px solid rgb(204, 158, 88)";
+input2.style.borderRadius="10px"
+input2.style.background="White";
+input2.style.color="Black";
+input2.style.padding="10px";
+
+emailManger.appendChild(label);
+emailManger.appendChild(input);
+
+passwordManger.appendChild(label2);
+passwordManger.appendChild(input2);
+
+form.appendChild(emailManger);
+form.appendChild(passwordManger)
+
+div.appendChild(form);
+div.appendChild(loginBtn);
+second.appendChild(div);
+
+div.style.display="flex";
+div.style.justifyContent="center";
+div.style.alignItems="center";
+div.style.flexDirection="column";
+div.style.gap="20px";
+div.style.background="White";
+div.style.height="50vh";
+div.style.width="30vw"
+div.style.borderRadius="20px";
+div.style.border=" 3px solid rgb(204, 158, 88)";
+
+form.style.display="flex";
+form.style.flexDirection="column";
+form.style.gap="30px";
+
+emailManger.style.display="flex";
+emailManger.style.flexDirection="row";
+emailManger.style.gap="68px";
+
+passwordManger.style.display="flex";
+passwordManger.style.flexDirection="row";
+passwordManger.style.gap="20px";
+
+
+
+})
