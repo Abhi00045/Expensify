96e4043d51aa618e8feb8cd094715a8226e8cd66
Abhi00045 <128639147+Abhi00045@users.noreply.github.com>
2024-06-17 22:30:29 +0530

    implemented somewhat js

--- signup.js
+++ signup.js
@@ -6,17 +6,17 @@ let cnPassword = document.getElementById("confirmPassword");
 let submit=document.getElementById("btn");
 
 submit.addEventListener("click",function(){
-    if(Fname.value===""){
-alert("Enter the Name");
-    }
-    if(email.value=" "){
-alert("Enter the Email");
-    }
-    else if(password.value=" "){
+    if(Fname.value ===""){
+        alert("Enter the Name");
+            }
+    if(email.value ===""){
+        alert("Enter the Email");
+            }
+    if(password.value ===""){
         alert("Enter the password");
             }
-           else if(cnPassword.value=" " && cnPassword.value!=password.value){
-                alert("Write the password");
-                    }
+    if(cnPassword.value!=password.value){
+        alert("Password doesn't match");
+            }
 })
 
