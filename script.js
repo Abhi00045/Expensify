26965ceaa982a80866e2865c96c50c4bb70e8820
abhi <rajidiabhinavreddy@gmail.com>
2024-09-02 18:43:37 +0530

    done with signup details

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ let third = document.getElementById("third");
 let fourth = document.getElementById("fourth");
 let loginLink = document.getElementById("loginLink");
 let loginBtn = document.getElementById("loginBtn");
-let signupBtn = document.getElementById("signupBtn");
+let form = document.getElementById("form");
+// let data= JSON.parse(localStorage.getItem("userInfo")) || [];
+// console.log(data)
+let data =[];
 
 window.addEventListener("load",(e)=>{
     third.style.display="none";
@@ -50,21 +53,25 @@ loginBtn.addEventListener("click",(e)=>{
 }
 })
 
-signupBtn.addEventListener("click",(e)=>{
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
-    console.log(name,email,password);
-
+    // console.log(name,email,password);
+    // console.log({name , email , password})
     if(name == "" || email == "" || password == ""){
         alert("please enter name , email and password");
+    }else{
+        localStorage.setItem("userInfo" , JSON.stringify({name , email , password}));
+         let SignupDetails={Name : name,
+            Email:email,
+            Password:password
+         };
+         data.push(SignupDetails);
+         console.log(data);  
     }
-    else{
-        localStorage.setItem("name",name);
-        localStorage.setItem("email",email);
-        localStorage.setItem("password",password);
-        third.style.display="none";
-        fourth.style.display="none";
-        second.style.display="block";
-    }
+    // name="";
+    // email="";
+    // password="";
 })
